b31cf073ff863011bdcbbfd8e47e893301ec04f7
Quentin ADADAIN <quentin.adadain@gmail.com>
2022-09-15 00:56:15 +0200

    Add index and show for tags and add symfony profiler for toolbar

--- portfolio/config/bundles.php
+++ portfolio/config/bundles.php
@@ -11,4 +11,5 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     FOS\CKEditorBundle\FOSCKEditorBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
