b772440d6949c6e906184cf1379d5ae2674340a4
Quentin ADADAIN <quentin.adadain@gmail.com>
2022-09-18 22:21:10 +0200

    Add knp paginator bundle and improve fixture's post

--- portfolio/config/bundles.php
+++ portfolio/config/bundles.php
@@ -12,4 +12,5 @@ return [
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     FOS\CKEditorBundle\FOSCKEditorBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
 ];
--- portfolio/src/Controller/BlogController.php
+++ portfolio/src/Controller/BlogController.php
@@ -5,8 +5,10 @@ namespace App\Controller;
 use App\Entity\Post;
 use App\Entity\Tag;
 use Doctrine\ORM\EntityManagerInterface;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Twig\Environment;
 
@@ -23,10 +25,15 @@ class BlogController extends AbstractController
     }
 
     #[Route(path: self::ROUTE_PATH_BLOG, name: self::ROUTE_NAME_BLOG, methods: [HttpFoundation\Request::METHOD_GET])]
-    public function handle(): HttpFoundation\Response
+    public function handle(PaginatorInterface $paginator, Request $request): HttpFoundation\Response
     {
         $posts = $this->em->getRepository(Post::class)->findAll();
         $tags = $this->em->getRepository(Tag::class)->findAll();
+        $posts = $paginator->paginate(
+            $posts,
+            $request->query->getInt('page', 1),
+            9,
+        );
 
         return new HttpFoundation\Response(content: $this->renderer->render(name: self::TEMPLATE, context: [
             'posts' => $posts,
